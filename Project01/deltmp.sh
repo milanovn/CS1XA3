61415f7b2e332853f40b596784746de309a737b4
milanovn <milanovn@mcmaster.ca>
2019-02-23 19:46:35 +0000

    Added deltmp to project_analyze

--- Project01/deltmp.sh
+++ Project01/deltmp.sh
@@ -2,7 +2,6 @@
 IFS='$\n'
 for file in $(git ls-files . --exclude-standard --others | grep '\.tmp')
 do
-  echo $file
   git clean -f "$file"
 done
 IFS=
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -3,6 +3,9 @@ read -p "Do you wish to execute todo.sh or file_count.sh? (Enter file name) " ch
 if [ $choice == 'todo.sh' ]; then
   ./todo.sh
 fi
-if [ $choice = 'file_count.sh' ]; then
+if [ $choice == 'file_count.sh' ]; then
   ./file_count.sh
+fi
+if [ $choice == 'deltmp.sh' ]; then
+  ./deltmp.sh
 fi
