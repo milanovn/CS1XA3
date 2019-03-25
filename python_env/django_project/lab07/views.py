5250f4ac94e9c7bf7773d57e1c91afe7f842ba5b
milanovn <milanovn@mcmaster.ca>
2019-03-25 01:17:59 +0000

    lab7 post and response

--- python_env/django_project/lab07/views.py
+++ python_env/django_project/lab07/views.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 def authentication(request):
 	name=request.POST.get("name","")
 	password=request.POST.get("password","")
-	#if name!="Jimmy" and password!="Hendrix":
-	#	return (HttpResponse("Bad User Name"))
-#	else:
-#		return (HttpResponse("Cool"))
-	return HttpResponse(name+password)
+	if name!="Jimmy" and password!="Hendrix":
+		return (HttpResponse("Bad User Name"))
+	else:
+		return (HttpResponse("Cool"))
 # Create your views here.
