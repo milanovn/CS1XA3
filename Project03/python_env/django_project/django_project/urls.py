04bf3533959052b75a6980a382f4a9c978ecbe88
milanovn <milanovn@mcmaster.ca>
2019-04-18 20:20:44 +0000

    Fixed routing with urls.py

--- Project03/python_env/django_project/django_project/urls.py
+++ Project03/python_env/django_project/django_project/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
-#Routed from  e/milanovn/casinoapp/
+from django.urls import path, include
+root = 'e/milanovn/'
 urlpatterns = [
-    path(),
+    path(root+'casinoapp/', include('casinoapp.urls')),
 ]
