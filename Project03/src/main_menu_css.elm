7234e811a61a6e8ce54f0e7208effad87dd9ff98
milanovn <milanovn@mcmaster.ca>
2019-04-24 06:43:11 +0000

    css for main_menu

---
+++ Project03/src/main_menu_css.elm
@@ -0,0 +1,73 @@
+import Browser
+import Http exposing (..)
+import Browser.Navigation exposing (load)
+import Html exposing (..)
+import Json.Decode as JDecode
+import Html.Attributes exposing (..)
+import Debug exposing (toString)
+import Html.Events exposing (..)
+
+main = Browser.element{init=init,update=update,subscriptions=\_->Sub.none,view=view}
+
+type alias Model = {username:String, points:Int,error:String}
+
+type Msg = LoadRoullete
+           | InfoResponse (Result Http.Error Model)
+           | Logout
+           | LogoutResponse(Result Http.Error String)
+init : () -> (Model,Cmd Msg)
+init _ = ({username="",points=0,error=""},requestInfo)
+
+stylesheet = node "link" [attribute "rel" "stylesheet",
+                          href "assets/menu.css"]
+                          []
+
+update : Msg -> Model -> (Model,Cmd Msg)
+update msg model = case msg of
+                        LoadRoullete -> (model,load "http://mac1xa3.ca/e/milanovn/static/roulette.html")
+                        InfoResponse result -> case result of
+                                               Ok updatedModel -> (updatedModel,Cmd.none)
+                                               Err error -> (handleError model error,Cmd.none)
+                        Logout -> (model,logout)
+                        LogoutResponse result -> case result of
+                                          Ok "Successfully Logged Out" -> (model,load "http://mac1xa3.ca/e/milanovn/static/login_menu.html")
+                                          Ok _                         -> (model,Cmd.none)
+                                          Err error -> (model,Cmd.none)
+view : Model -> Html Msg
+view model = div [] [stylesheet
+                    ,div [class "accordion"] [section [class "accordion-item"] [h1 [] [text "User:"], div [class "accordion-item-content"] [p [] [text model.username]]]
+                                             ,section [class "accordion-item"] [h1 [] [text "Points:"],div [class "accordion-item-content"] [p [] [text (Debug.toString model.points)]]]
+                                             ,section [class "accordion-item"] [h1 [] [text "Play:"],div [class "accordion-item-content"] [p [] [button [onClick LoadRoullete] [text "Roulette"]]]]
+                                             ,section [class "accordion-item",onClick Logout] [h1 [] [text "Logout"]]
+                                             ]
+                    ]
+requestInfo : Cmd Msg
+requestInfo = Http.get{url="https://mac1xa3.ca/e/milanovn/casinoapp/requestPoints/",
+                       expect = Http.expectJson InfoResponse modelDecoder}
+logout : Cmd Msg
+logout = Http.get{url="https://mac1xa3.ca/e/milanovn/casinoapp/logout/",
+                       expect = Http.expectString LogoutResponse}
+modelDecoder : JDecode.Decoder Model
+modelDecoder = JDecode.map3 Model
+  (JDecode.field "username" JDecode.string)
+  (JDecode.field "points" JDecode.int)
+  (JDecode.field "error" JDecode.string)
+
+handleError : Model -> Http.Error -> Model
+handleError model error =
+    case error of
+        Http.BadUrl url ->
+            { model | error = "bad url: " ++ url }
+
+        Http.Timeout ->
+            { model | error = "timeout" }
+
+        Http.NetworkError ->
+            { model | error = "network error" }
+
+        Http.BadStatus i ->
+            { model | error = "bad status " ++ String.fromInt i }
+
+        Http.BadBody body ->
+            { model | error = "bad body " ++ body }
+
