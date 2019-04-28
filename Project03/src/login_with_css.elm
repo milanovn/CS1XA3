68cde88af79d8bb1504369e8ab17e527c3f36d53
milanovn <milanovn@mcmaster.ca>
2019-04-28 02:41:27 +0000

    Comments for login_with_css.elm

--- Project03/src/login_with_css.elm
+++ Project03/src/login_with_css.elm
@@ -13,6 +13,7 @@ type alias Model = {username:String,password:String,error:String}
 
 root = "http://mac1xa3.ca/e/milanovn/"
 
+--CSS stylesheet for login.
 stylesheet = node "link" [attribute "rel" "stylesheet",
                           href "assets/login.css"]
                           []
@@ -29,15 +30,15 @@ init _ = ({username="",password="",error=""},Cmd.none)
 
 update : Msg -> Model -> (Model,Cmd Msg)
 update msg model = case msg of
-                        UpdateName givenName -> ({model|username=givenName},Cmd.none)
-                        UpdatePassword givenPassword -> ({model|password=givenPassword},Cmd.none)
-                        CreateUser -> (model,createUserPost model)
-                        GotCreateResponse result -> case result of
+                        UpdateName givenName -> ({model|username=givenName},Cmd.none) --This gets called onInput on text box and updates our model username.
+                        UpdatePassword givenPassword -> ({model|password=givenPassword},Cmd.none) --Same as the above except for the model password.
+                        CreateUser -> (model,createUserPost model) --Calls a CMD msg to send the model to the server and create a new user in the database.
+                        GotCreateResponse result -> case result of --Error if input fields are invalid.
                                                       Ok "Please provide a username" -> ({model|error="Username required."},Cmd.none)
                                                       Ok "Please provide a password" -> ({model|error="Password required."},Cmd.none)
                                                       Ok _ -> ({model|error="USER CREATED"},Cmd.none)
                                                       Err error -> (handleError model error, Cmd.none)
-                        Login -> (model,loginUserPost model)
+                        Login -> (model,loginUserPost model) --Requests login to main menu.
                         LoginResponse result -> case result of
                                                   Ok "Invalid login" -> ({model|error="Invalid login"},Cmd.none)
                                                   Ok "Authentication Failed" ->({model|error="Authentication Failed"},Cmd.none)
@@ -51,20 +52,20 @@ view model = div [] [stylesheet,
                                          ,button [type_ "submit",onClick CreateUser] [text "Create user"],button [type_ "response"] [text model.error]]
                     ]
 
-createUserPost : Model -> Cmd Msg
+createUserPost : Model -> Cmd Msg --Encodes the model and sends it as a json body to the url which will then createa a user in the database if valid.
 createUserPost model = Http.post {url = "https://mac1xa3.ca/e/milanovn/casinoapp/addUser/"
                               ,body=Http.jsonBody <| (userCredentials model)
                               ,expect = Http.expectString GotCreateResponse}
 
-loginUserPost : Model -> Cmd Msg
+loginUserPost : Model -> Cmd Msg --Also encodes the model but this time requests a login  with the given inputs.
 loginUserPost model = Http.post{url = "https://mac1xa3.ca/e/milanovn/casinoapp/loginUser/"
                                ,body=Http.jsonBody <| (userCredentials model)
                                ,expect = Http.expectString LoginResponse}
 
-userCredentials : Model -> JEncode.Value
+userCredentials : Model -> JEncode.Value --Encodes username and password in order to communicate with server.
 userCredentials model = JEncode.object [("username", JEncode.string model.username),("password", JEncode.string model.password)]
 
-
+--Error handling.
 handleError : Model -> Http.Error -> Model
 handleError model error =
     case error of
